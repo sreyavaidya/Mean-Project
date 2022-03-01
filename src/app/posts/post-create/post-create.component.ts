352a18448015ba270f3e0b169ef88c23ca1e06e4
Shreya Vaidya <shreya.vaidya37@gmail.com>
2022-03-01 21:18:58 +0545

    frontend form changes

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,13 +1,24 @@
-import { Component } from "@angular/core";
-
+import { PostsService } from './../posts.service';
+import { Component} from "@angular/core";
+import { NgForm } from "@angular/forms";
 @Component({
   selector: 'app-post-create',
-  templateUrl: './post-create.component.html'
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
-  newPost = 'NO CONTENT';
-  enteredValue = '';
-  onAddPost() {
-    this.newPost = this.enteredValue;
+  enteredTitle = '';
+  enteredContent = '';
+
+  constructor(public postsService: PostsService) {
+
+  }
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.postsService.addPost(form.value.title, form.value.content);
+    form.reset();
   }
 }
